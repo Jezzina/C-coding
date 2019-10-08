462350b4a75f43c1dc1d2c7aabaf6b20e55df57a
priyanshisharma <30956619+priyanshisharma@users.noreply.github.com>
2019-10-09 00:35:15 +0530

    Errors corrected
    Basic syntactical errors

--- newf2.cpp
+++ newf2.cpp
@@ -1,13 +1,13 @@
 #include <iostream>  
-using namespace std  
+using namespace std;  
 int main()  
- 
+{  
    int i,fact=1,number;    
   cout<<"Enter any Number: ";    
  cin>>number;    
-  for(i=1;i<=numberi++){    
+  for(i=1;i<=number;i++){    
       fact=fact*i;    
   }    
-  cout<<"Factorial of  <<number<<" is: "<<fact<<endl;  
+  cout<<"Factorial of " <<number<<" is: "<<fact<<endl;  
   return 0;  
 }  
